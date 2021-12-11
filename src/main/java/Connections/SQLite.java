42d77a46c4d62eff83e33408d45a81af706075e3
Ahmad Khalili <ahmadkhalili01@outlook.com>
2021-12-11 12:46:48 +0200

    Add support for Sqlite connections and resolve dependencies

---
+++ src/main/java/Connections/SQLite.java
@@ -0,0 +1,17 @@
+package Connections;
+
+import java.sql.*;
+
+public class SQLite {
+    protected static Connection instance;
+    protected static Connection getInstance(ConnectionInfo connectionInfo){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            instance = DriverManager.getConnection("jdbc:sqlite:D:\\name.db");
+            return instance;
+        }catch(Exception ex){
+            System.out.println(ex);
+            return null;
+        }
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,38 @@
+import Connections.Database;
+import Constants.ConnectionType;
+import Connections.*;
+
+import java.sql.*;
+import java.util.*;
+
 public class main {
     public static void main(String[] args) {
+        //Database connection = new Connection();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter username: ");
+        String user = sc.nextLine();
+
+        System.out.println("Enter password: ");
+        String pass = sc.nextLine();
+
+        System.out.println("Enter port number: ");
+        String port = sc.nextLine();
+
+        System.out.println("Enter the database's name: ");
+        String dbName = sc.nextLine();
+
+        ConnectionInfo connectionInfo = new ConnectionInfo(user, pass, port, dbName);
+
+        System.out.println("Select database type: \n" +
+                "1- MySql\n" + "2- Sqlite\n" + "3- Oracle" + "\nChoice: ");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case (1):
+                connection = connection.createConnection(ConnectionType.MySql, connectionInfo);
+        }
+        //Database.exportData(ConnectionType.MySql);
+        connection.displayTables(ConnectionType.MySql);
+        connection.closeConnection(ConnectionType.MySql);
     }
 }
