dfe371277633fa584ac45f14f8ec98053e53dbfb
Ahmad Khalili <ahmadkhalili01@outlook.com>
2021-12-14 21:27:02 +0200

    Add profile creation methods and code touch ups

--- src/main/java/Connections/MySQL.java
+++ src/main/java/Connections/MySQL.java
@@ -1,11 +1,23 @@
 package Connections;
 
+import com.opencsv.CSVWriter;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
 
 public class MySQL{
+    protected static ConnectionInfo connectionInfo1;
     protected static Connection instance;
+    protected static Statement statement;
     protected static Connection getInstance(ConnectionInfo connectionInfo){
         try{
+            connectionInfo1 = connectionInfo;
             Class.forName("com.mysql.cj.jdbc.Driver");
             instance =DriverManager.getConnection("jdbc:mysql://localhost:" + connectionInfo.port + "/"
                     + connectionInfo.dbName,connectionInfo.username, connectionInfo.password);
@@ -16,51 +28,186 @@ public class MySQL{
         }
     }
 
-    protected static void exportData(){
-        String[] filePath = new String[]{"/bin/ln","-s","C:\\Program Files\\MySQL\\MySQL\\Server\\s+8.0\\bin -u root -p  dbms"};
-        String executeCmd = filePath + "C:\\dump.sql";
-        try{
-            Process runtimeProcess = Runtime.getRuntime().exec(executeCmd);
-            int status = runtimeProcess.waitFor();
-            if (status == 0){
-                System.out.println("Success");
-            }
-            else{
-                System.out.println("Fail");
-            }
-        }
-        catch (Exception ex){
-            System.out.println("exception: " + ex.getMessage());
-        }
-    }
-
-    protected static void displayTables(){
+    public static void takeSnapshot() {
         try {
-            ResultSet rs = null;
-            DatabaseMetaData meta = (DatabaseMetaData) instance.getMetaData();
-            rs = meta.getTables(null, null, null, new String[]{
-                    "TABLE"
-            });
-            int count = 0;
-            System.out.println("Table Names: ");
-            while(rs.next()){
-                String tableName = rs.getString("TABLE_NAME");
-                System.out.println(tableName);
-                count++;
+            Process exec = Runtime.getRuntime().exec(new String[]{"cmd.exe","/c",
+                    "\"C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysqldump\" -u" +
+                    connectionInfo1.username + " -p" +
+                    connectionInfo1.password + " " + connectionInfo1.dbName + " > C:\\Users\\ahmad\\Documents\\" + connectionInfo1.dbName +".sql"});
+            if(exec.waitFor()==0)
+            {
+                //normally terminated, a way to read the output
+                InputStream inputStream = exec.getInputStream();
+                byte[] buffer = new byte[inputStream.available()];
+                inputStream.read(buffer);
+
+                String str = new String(buffer);
+                System.out.println(str);
             }
-            System.out.println(count + " Rows in set ");
-        }
-        catch (SQLException ex){
+            else
+            {
+                // abnormally terminated, there was some problem
+                //a way to read the error during the execution of the command
+                InputStream errorStream = exec.getErrorStream();
+                byte[] buffer = new byte[errorStream.available()];
+                errorStream.read(buffer);
 
+                String str = new String(buffer);
+                System.out.println(str);
+
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    protected static void closeMySql(){
+    protected static void exportData(Connection connection){
+        try {
+            Process exec = Runtime.getRuntime().exec(new String[]{"cmd.exe","/c",
+                    "\"C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysqldump\" -u" +
+                            connectionInfo1.username + " -p" +
+                            connectionInfo1.password + " " + connectionInfo1.dbName + " > C:\\Users\\ahmad\\Documents\\" + connectionInfo1.dbName +".sql"});
+            if(exec.waitFor()==0)
+            {
+                //normally terminated, a way to read the output
+                InputStream inputStream = exec.getInputStream();
+                byte[] buffer = new byte[inputStream.available()];
+                inputStream.read(buffer);
+
+                String str = new String(buffer);
+                System.out.println(str);
+            }
+            else
+            {
+                // abnormally terminated, there was some problem
+                //a way to read the error during the execution of the command
+                InputStream errorStream = exec.getErrorStream();
+                byte[] buffer = new byte[errorStream.available()];
+                errorStream.read(buffer);
+
+                String str = new String(buffer);
+                System.out.println(str);
+
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected static void importSnapshot(String s) {
+        try {
+            Process exec = Runtime.getRuntime().exec(new String[]{"cmd.exe","/c",
+                    "\"C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysql\" -u" +
+                            connectionInfo1.username + " -p" +
+                            connectionInfo1.password + " " + connectionInfo1.dbName + " < C:\\Users\\ahmad\\Documents\\" + s});
+            if(exec.waitFor()==0)
+            {
+                //normally terminated, a way to read the output
+                InputStream inputStream = exec.getInputStream();
+                byte[] buffer = new byte[inputStream.available()];
+                inputStream.read(buffer);
+
+                String str = new String(buffer);
+                System.out.println(str);
+            }
+            else
+            {
+                // abnormally terminated, there was some problem
+                //a way to read the error during the execution of the command
+                InputStream errorStream = exec.getErrorStream();
+                byte[] buffer = new byte[errorStream.available()];
+                errorStream.read(buffer);
+
+                String str = new String(buffer);
+                System.out.println(str);
+
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+        protected static void displayTables(){
+        try {
+            DatabaseMetaData meta = instance.getMetaData();
+            ResultSet rs = meta.getTables(null, null,null,new String[]{"TABLE"});
+            while(rs.next()){
+                System.out.println(rs.getString("TABLE_NAME"));
+            }
+        }
+        catch (SQLException ex){
+            System.out.println("SQLException: " + ex.getMessage());
+        }
+    }
+    //INSERT STATEMENT
+    public static boolean executeInsertStmt(String insertStmt) {
+        try {
+            statement = instance.createStatement();
+            int recordInserted = statement.executeUpdate(insertStmt);
+            return recordInserted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    //SELECT STATEMENT(Shahd)
+    public static void selectStatement(String tableName){
+        try {
+            PreparedStatement ps = instance.prepareStatement("SELECT * from " + tableName);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                System.out.println(rs.getString(1));
+            }
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+    //UPDATE STATEMENT(Farah)
+    public void updateStatement(Integer id){
+        String query=" UPDATE tableName SET = ? = ? where id = ?" ;
         try{
-            instance.close();
-        }
-        catch (SQLException ex){
-            System.out.println(ex.getMessage());
+            Statement stmt = instance.createStatement();
+            // execute the delete statement
+            stmt.executeUpdate(query);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
+    //DELETE STATEMENT(Farah)
+    public static void deleteStatement(String tableName, int id){
+        String sql = "DELETE FROM " + tableName + " where id = ?";
+        try{
+            PreparedStatement stmt = instance.prepareStatement(sql);
+            // set the corresponding param
+            stmt.setInt(1, id);
+            // execute the delete statement
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void closeConnection() {
+        if (instance != null) {
+            try {
+                instance.close();
+                System.out.println("Connection Closed!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,38 +1,88 @@
+import Authentication.Lab1Viewer;
 import Connections.Database;
 import Constants.ConnectionType;
 import Connections.*;
+import Logger.Logger;
+import Authentication.Lab1Frame;
 
+import javax.swing.*;
+import java.io.FileNotFoundException;
 import java.sql.*;
 import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-        //Database connection = new Connection();
         Scanner sc = new Scanner(System.in);
+        Lab1Viewer.loginAuth();
+        String startCommand = sc.next();
+        if (startCommand.equals("start")) {
+            if (Lab1Frame.userType.equals("1")) {
 
-        System.out.println("Enter username: ");
-        String user = sc.nextLine();
+                Database database = new Database();
+                ConnectionInfo connectionInfo = null;
 
-        System.out.println("Enter password: ");
-        String pass = sc.nextLine();
+                System.out.println("1- Choose existing profile\n" +
+                        "2- Create new Profile\n" +
+                        "Choice: ");
+                int choice1 = sc.nextInt();
 
-        System.out.println("Enter port number: ");
-        String port = sc.nextLine();
+                if (choice1 == 1) {
+                    database.showProfiles();
+                    System.out.println("Choice: ");
+                    String choice2 = sc.next();
+                    connectionInfo = database.readProfile(choice2);
+                }
+                if (choice1 == 2) {
+                    System.out.println("Enter connection info in this format (user,password,port,name): ");
+                    String info = sc.next();
+                    database.createProfile(info);
+                }
 
-        System.out.println("Enter the database's name: ");
-        String dbName = sc.nextLine();
+                System.out.println("Select database type: \n" +
+                        "1- MySql\n" + "2- Sqlite\n" + "3- Oracle" + "\nChoice: ");
+                int choice3 = sc.nextInt();
+                if (choice3 == 1) {
+                    database.createConnection(ConnectionType.MySql, connectionInfo);
+                    System.out.println("1- View Tables\n" +
+                            "2- Take Snapshot\n" +
+                            "3- Import Data\n" +
+                            "4- Export Data\n" +
+                            "5- Edit Profile\n" +
+                            "6- Remove Profile\n" +
+                            "7- Exit");
+                    int choice4 = sc.nextInt();
+                    if (choice4 == 1) {
+                        database.displayTables(ConnectionType.MySql);
+                        System.out.println("1- Select Table");
+                        int choiceTables = sc.nextInt();
+                        if (choiceTables == 1) {
+                            System.out.println("Enter Table Name: ");
+                            String table = sc.next();
+                            database.executeSelectStatement(ConnectionType.MySql, table);
+                        }
+                    }
+                    if (choice4 == 2) {
+                        database.takeSnapshot(ConnectionType.MySql);
+                    }
+                    if (choice4 == 5) {
 
-        ConnectionInfo connectionInfo = new ConnectionInfo(user, pass, port, dbName);
-
-        System.out.println("Select database type: \n" +
-                "1- MySql\n" + "2- Sqlite\n" + "3- Oracle" + "\nChoice: ");
-        int choice = sc.nextInt();
-        switch (choice) {
-            case (1):
-                connection = connection.createConnection(ConnectionType.MySql, connectionInfo);
+                    }
+            /*if (choice2 == 4){
+                database.exportData(ConnectionType.MySql);
+            }*/
+                    if (choice4 == 7) {
+                        database.closeConnection(ConnectionType.MySql);
+                    }
+                }
+                if (choice3 == 2) {
+                    System.out.println("Not supported yet");
+                }
+                if (choice3 == 3) {
+                    System.out.println("Not supported yet");
+                }
+                System.out.println(Lab1Frame.userType);
+                Logger.SaveLogs();
+            }
         }
-        //Database.exportData(ConnectionType.MySql);
-        connection.displayTables(ConnectionType.MySql);
-        connection.closeConnection(ConnectionType.MySql);
     }
 }
